51567ba2e018903993837c00e78f65491ac78b81
Martin Trojer <martin.trojer@gmail.com>
2014-02-16 10:45:51 +0000

    new libtorrent version

--- puppet/modules/build-libtorrent-rasterbar/files/build_libtorrent.sh
+++ puppet/modules/build-libtorrent-rasterbar/files/build_libtorrent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.16.14"
+VERSION="0.16.15"
 
 wget -O libtorrent-rasterbar-$VERSION.tar.gz "http://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-$VERSION.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Flibtorrent%2Ffiles%2Flibtorrent%2F&ts=1392480392&use_mirror=netcologne"
 tar zxf libtorrent-rasterbar-$VERSION.tar.gz
