4875b31e75529a821da3abcb712e61e5c69d7821
Martin Trojer <martin.trojer@gmail.com>
2014-05-10 18:59:39 +0100

    always apt-get update

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,7 @@
+stage { 'preinstall': before => Stage['main'] }
+class apt_get_update { exec {'/usr/bin/apt-get -y update':} }
+class { 'apt_get_update': stage => preinstall }
+
 include dotfiles
 
 include nodejs
