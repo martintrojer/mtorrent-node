b383ef3d630aa806f2d95de8944a286d7c94e630
Martin Trojer <martin.trojer@gmail.com>
2014-11-09 14:40:37 +0000

    make it work with libtorrent 1.0+

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM ubuntu:trusty
 
+RUN apt-get -y update
 RUN apt-get -y install nodejs libtorrent-rasterbar7
 
 ADD mtorrent.js mtorrent.js
 ADD public public
 ADD node_modules node_modules
-ADD libtorrent libtorrent
-ADD libtorrent-rasterbar.so.7.0.0 /usr/lib/libtorrent-rasterbar.so.7.0.0
-ADD libtorrent-rasterbar.so.7 /usr/lib/libtorrent-rasterbar.so.7
+ADD node_modules/node-libtorrent-mt/build/Release/libtorrent.node libtorrent/libtorrent.node
+ADD libtorrent-rasterbar.so.8.0.0 /usr/lib/libtorrent-rasterbar.so.8.0.0
+ADD libtorrent-rasterbar.so.8 /usr/lib/libtorrent-rasterbar.so.8
 
 EXPOSE 1337
 
--- puppet/modules/build-libtorrent-rasterbar/files/build_libtorrent.sh
+++ puppet/modules/build-libtorrent-rasterbar/files/build_libtorrent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.16.16"
+VERSION="1.0.2"
 
 wget -O libtorrent-rasterbar-$VERSION.tar.gz "http://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-$VERSION.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Flibtorrent%2Ffiles%2Flibtorrent%2F&ts=1392480392&use_mirror=netcologne"
 tar zxf libtorrent-rasterbar-$VERSION.tar.gz
@@ -8,6 +8,6 @@ cd libtorrent-rasterbar-$VERSION
 ./configure --prefix=/usr
 make install
 cd /vagrant
-cp /usr/lib/libtorrent-rasterbar.so.7.0.0 .
-strip libtorrent-rasterbar.so.7.0.0
-ln -s libtorrent-rasterbar.so.7.0.0 libtorrent-rasterbar.so.7
+cp /usr/lib/libtorrent-rasterbar.so.8.0.0 .
+strip libtorrent-rasterbar.so.8.0.0
+ln -s libtorrent-rasterbar.so.8.0.0 libtorrent-rasterbar.so.8
